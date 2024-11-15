09872ac32a05148dd13e567b8b708fb744af555b
Robbin Baauw <robbin.baauw@gmail.com>
2024-11-15 13:27:22 +0100

    Fzn generatoin

--- pumpkin-solver/benches/generate-fzn.py
+++ pumpkin-solver/benches/generate-fzn.py
@@ -1,26 +1,43 @@
 import itertools
 import multiprocessing
+import shutil
 import subprocess
+from collections import defaultdict
 from multiprocessing.pool import ThreadPool
 from pathlib import Path
+from typing import Optional
 
 BENCH_DIR = Path(__file__).parent / "minizinc-benchmarks"
+BENCH_SET_DIR = Path(__file__).parent / "benchmark-set"
+
 PUMPKIN_SOLVER = Path(__file__).parent.parent.parent / "minizinc" / "pumpkin-linear-ineq.msc"
+
+TOTAL_SAMPLES = 5000
 
 thread_pool = ThreadPool()
 
 
-def worker_generate(mzn_path: Path, dzn_path: Path):
-    dzn_name = dzn_path.stem
+def find_fzn_name_path(mzn_path: Path, dzn_path: Optional[Path]):
+    if dzn_path is None:
+        fzn_name = f"{mzn_path.stem}.fzn"
+        fzn_path = mzn_path.parent / fzn_name
+    else:
+        dzn_name = dzn_path.stem
 
-    dzn_parent_path = dzn_path.parent
-    while mzn_path.parent != dzn_parent_path:
-        # dzn in subdir
-        dzn_name = f"{dzn_parent_path.stem}_{dzn_name}"
-        dzn_parent_path = dzn_parent_path.parent
+        dzn_parent_path = dzn_path.parent
+        while mzn_path.parent != dzn_parent_path:
+            # dzn in subdir
+            dzn_name = f"{dzn_parent_path.stem}_{dzn_name}"
+            dzn_parent_path = dzn_parent_path.parent
 
-    fzn_name = f"{mzn_path.stem}_{dzn_name}.fzn"
-    fzn_path = mzn_path.parent / fzn_name
+        fzn_name = f"{mzn_path.stem}_{dzn_name}.fzn"
+        fzn_path = mzn_path.parent / fzn_name
+
+    return fzn_name, fzn_path
+
+
+def worker_generate(mzn_path: Path, dzn_path: Optional[Path]):
+    fzn_name, fzn_path = find_fzn_name_path(mzn_path, dzn_path)
 
     if fzn_path.exists():
         print(f"({multiprocessing.current_process().name}) Already exists, skip!")
@@ -28,8 +45,9 @@ def worker_generate(mzn_path: Path, dzn_path: Path):
 
     print(f"({multiprocessing.current_process().name}) Generating {fzn_path}")
     cmd = ["minizinc", "-c", str(mzn_path.resolve()),
-            str(dzn_path.resolve()), "--solver", str(PUMPKIN_SOLVER.resolve()),
+            None if dzn_path is None else str(dzn_path.resolve()), "--solver", str(PUMPKIN_SOLVER.resolve()),
             "--fzn", str(fzn_path.resolve())]
+    cmd = list(filter(lambda c: c is not None, cmd))
 
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
@@ -43,14 +61,17 @@ def worker_generate(mzn_path: Path, dzn_path: Path):
 
 def search_dir(dir_path: Path):
     mzn_files = list(dir_path.rglob("./*.mzn"))
+
     dzn_files = list(dir_path.rglob("./*.dzn"))
+    if len(mzn_files) > 0 and len(dzn_files) == 0:
+        dzn_files = [None]
 
     model_data_combos = itertools.product(mzn_files, dzn_files)
     for (model, data) in model_data_combos:
         thread_pool.apply_async(worker_generate, (model, data))
 
 
-if __name__ == "__main__":
+def generate_fzn():
     for f in BENCH_DIR.iterdir():
         if f.is_dir():
             search_dir(f)
@@ -58,3 +79,55 @@ if __name__ == "__main__":
     thread_pool.close()
     thread_pool.join()
 
+
+def select_fzn():
+    mzn_dzn_files = []
+
+    for f in BENCH_DIR.iterdir():
+        if f.is_dir():
+            mzn_files = list(f.rglob("./*.mzn"))
+            dzn_files = list(f.rglob("./*.dzn"))
+            if len(mzn_files) > 0 and len(dzn_files) == 0:
+                dzn_files = [None]
+
+            mzn_dzn_files.extend([(mzn, dzn_files) for mzn in mzn_files])
+
+    mzn_dzn_files = list(filter(lambda i: len(i[1]) != 0, mzn_dzn_files))
+
+    mzn_fzn_paths = defaultdict(list)
+    bench_idx = defaultdict(lambda: 0)
+
+    def add_next_file(mzn, dzns):
+        dzn_idx = bench_idx[mzn_idx]
+
+        if dzn_idx >= len(dzns):
+            return
+        else:
+            bench_idx[mzn_idx] += 1
+
+            _, fzn_path = find_fzn_name_path(mzn, dzns[dzn_idx])
+            if not fzn_path.exists():
+                return add_next_file(mzn, dzns)
+
+            mzn_fzn_paths[mzn.parent.name].append(fzn_path)
+
+    while sum(map(lambda v: len(v), mzn_fzn_paths.values())) < TOTAL_SAMPLES:
+        for mzn_idx, (mzn, dzns) in enumerate(mzn_dzn_files):
+            add_next_file(mzn, dzns)
+
+    for (mzn_dir, fzn_paths) in mzn_fzn_paths.items():
+        new_dir = BENCH_SET_DIR / mzn_dir
+        new_dir.mkdir()
+
+        for path in fzn_paths:
+            new_path = new_dir / path.name
+            if new_path.exists():
+                print(f"Skipping copy of {new_path.name}")
+                continue
+
+            shutil.copy(path, new_path)
+
+
+if __name__ == "__main__":
+    # generate_fzn()
+    select_fzn()
